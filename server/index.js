3deb757082a5264ba2d40354ddc42b0f60f06db0
Joy Chandra Uday <joychandraud@gmail.com>
2025-02-11 16:17:02 +0600

    updated cors ocation

--- server/index.js
+++ server/index.js
@@ -13,6 +13,7 @@ const corsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:5174",
+    "http://localhost:3000",
     "https://joychandrauday.web.app",
   ],
   credentials: true,
